12f492c58304ce10fb8f782619f8fbd601bf6a43
edgedoggo <152747753+edgedoggo@users.noreply.github.com>
2023-12-02 20:31:17 -0500

    Update client-side.js

--- earthquake/scripts/client-side.js
+++ earthquake/scripts/client-side.js
@@ -1,15 +1,30 @@
 Hooks.on('socketlib.ready', () => {
+  console.log("Socketlib is ready. Listening for screen wiggle event...");
+
   game.socket.on('module.screenWiggle', (data) => {
-    if (data.start) {
-      window.screenWiggle(data.wiggleAmount, data.wiggleDuration);
+    console.log("Received screen wiggle event", data);
+
+    if (data && data.start) {
+      try {
+        window.screenWiggle(data.wiggleAmount, data.wiggleDuration);
+      } catch (error) {
+        console.error("Error in screenWiggle function:", error);
+      }
     } else {
+      console.log("Screen wiggle event data is invalid or 'start' is false.");
       // Handle the end of the screen wiggle if needed
     }
   });
 });
 
-// Define the screenWiggle function globally
 window.screenWiggle = async function (wiggleAmount, wiggleDuration) {
+  console.log(`Starting screen wiggle with amount ${wiggleAmount} and duration ${wiggleDuration}`);
+
+  if (!canvas || !canvas.stage) {
+    console.error("Canvas or canvas.stage is undefined.");
+    return;
+  }
+
   const originalPosition = canvas.stage.pivot.clone();
   let isShaking = true;
 
@@ -31,5 +46,6 @@ window.screenWiggle = async function (wiggleAmount, wiggleDuration) {
 
   setTimeout(() => {
     isShaking = false;
+    console.log("Screen wiggle ended.");
   }, wiggleDuration);
 };
