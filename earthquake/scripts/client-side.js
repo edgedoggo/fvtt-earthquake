6db848cde139b0883982ab9290b97e21336e3d49
edgedoggo <152747753+edgedoggo@users.noreply.github.com>
2023-12-02 20:50:41 -0500

    Update client-side.js

--- earthquake/scripts/client-side.js
+++ earthquake/scripts/client-side.js
@@ -1,51 +1,49 @@
 Hooks.on('socketlib.ready', () => {
-  console.log("Socketlib is ready. Listening for screen wiggle event...");
+    console.log("Socketlib is ready. Listening for screen wiggle event...");
 
-  game.socket.on('module.screenWiggle', (data) => {
-    console.log("Received screen wiggle event", data);
+    game.socket.on('module.screenWiggle', (data) => {
+        console.log("Event listener triggered with data:", data);
 
-    if (data && data.start) {
-      try {
-        window.screenWiggle(data.wiggleAmount, data.wiggleDuration);
-      } catch (error) {
-        console.error("Error in screenWiggle function:", error);
-      }
-    } else {
-      console.log("Screen wiggle event data is invalid or 'start' is false.");
-      // Handle the end of the screen wiggle if needed
-    }
-  });
+        if (data && data.start) {
+            try {
+                window.screenWiggle(data.wiggleAmount, data.wiggleDuration);
+                console.log("screenWiggle function called with amount:", data.wiggleAmount, "and duration:", data.wiggleDuration);
+            } catch (error) {
+                console.error("Error in screenWiggle function:", error);
+            }
+        } else {
+            console.log("Screen wiggle event data is invalid or 'start' is false.");
+        }
+    });
 });
 
 window.screenWiggle = async function (wiggleAmount, wiggleDuration) {
-  console.log(`Starting screen wiggle with amount ${wiggleAmount} and duration ${wiggleDuration}`);
+    console.log(`Starting screen wiggle with amount ${wiggleAmount} and duration ${wiggleDuration}`);
 
-  if (!canvas || !canvas.stage) {
-    console.error("Canvas or canvas.stage is undefined.");
-    return;
-  }
-
-  const originalPosition = canvas.stage.pivot.clone();
-  let isShaking = true;
-
-  function animate() {
-    if (!isShaking) {
-      canvas.animatePan({ x: originalPosition.x, y: originalPosition.y });
-      return;
+    if (!canvas || !canvas.stage) {
+        console.error("Canvas or canvas.stage is undefined.");
+        return;
     }
 
-    const xOffset = (Math.random() * wiggleAmount - wiggleAmount / 2) | 0;
-    const yOffset = (Math.random() * wiggleAmount - wiggleAmount / 2) | 0;
+    const originalPosition = canvas.stage.pivot.clone();
+    const startTime = Date.now();
 
-    canvas.animatePan({ x: originalPosition.x + xOffset, y: originalPosition.y + yOffset });
+    function animate() {
+        const currentTime = Date.now();
+        const elapsedTime = currentTime - startTime;
 
-    requestAnimationFrame(animate);
-  }
+        if (elapsedTime >= wiggleDuration) {
+            canvas.animatePan({ x: originalPosition.x, y: originalPosition.y });
+            console.log("Screen wiggle ended.");
+            return;
+        }
 
-  animate();
+        const xOffset = (Math.random() * wiggleAmount - wiggleAmount / 2) | 0;
+        const yOffset = (Math.random() * wiggleAmount - wiggleAmount / 2) | 0;
 
-  setTimeout(() => {
-    isShaking = false;
-    console.log("Screen wiggle ended.");
-  }, wiggleDuration);
+        canvas.animatePan({ x: originalPosition.x + xOffset, y: originalPosition.y + yOffset });
+        requestAnimationFrame(animate);
+    }
+
+    animate();
 };
