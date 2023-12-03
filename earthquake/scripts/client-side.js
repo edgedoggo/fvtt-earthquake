e0f840ab1371b8fbfa152c33e1f6239ab6f8936d
edgedoggo <152747753+edgedoggo@users.noreply.github.com>
2023-12-02 20:15:32 -0500

    Update client-side.js

--- earthquake/scripts/client-side.js
+++ earthquake/scripts/client-side.js
@@ -1,11 +1,35 @@
 Hooks.on('socketlib.ready', () => {
-  game.socket.on('module.screenWiggle', (start) => {
-    if (start) {
-      screenWiggle();
+  game.socket.on('module.screenWiggle', (data) => {
+    if (data.start) {
+      window.screenWiggle(data.wiggleAmount, data.wiggleDuration);
     } else {
       // Handle the end of the screen wiggle if needed
     }
   });
 });
 
-// Add any other client-side code you need here
+// Define the screenWiggle function globally
+window.screenWiggle = async function (wiggleAmount, wiggleDuration) {
+  const originalPosition = canvas.stage.pivot.clone();
+  let isShaking = true;
+
+  function animate() {
+    if (!isShaking) {
+      canvas.animatePan({ x: originalPosition.x, y: originalPosition.y });
+      return;
+    }
+
+    const xOffset = (Math.random() * wiggleAmount - wiggleAmount / 2) | 0;
+    const yOffset = (Math.random() * wiggleAmount - wiggleAmount / 2) | 0;
+
+    canvas.animatePan({ x: originalPosition.x + xOffset, y: originalPosition.y + yOffset });
+
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+
+  setTimeout(() => {
+    isShaking = false;
+  }, wiggleDuration);
+};
